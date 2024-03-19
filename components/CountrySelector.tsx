e3474ef04dacfa2cf246f1db403f37cbb55f0b15
infi <106063205+InfiniteGwei@users.noreply.github.com>
2024-03-19 22:14:17 +0530

    Added flags

--- components/CountrySelector.tsx
+++ components/CountrySelector.tsx
@@ -1,20 +1,56 @@
 import React from "react";
 import Image from 'next/image';
+import Link from "next/link"
 
-import CN from '../public/flags/CN.svg';
+import ar from '../public/flags/how-to-buy/ar.svg';
+import au from '../public/flags/how-to-buy/au.svg';
+import bg from '../public/flags/how-to-buy/bg.svg';
+import br from '../public/flags/how-to-buy/br.svg';
+import cm from '../public/flags/how-to-buy/cm.svg';
+import cn from '../public/flags/how-to-buy/cn.svg';
+import co from '../public/flags/how-to-buy/co.svg';
+import cz from '../public/flags/how-to-buy/cz.svg';
+import de from '../public/flags/how-to-buy/de.svg';
+import ec from '../public/flags/how-to-buy/ec.svg';
+import eg from '../public/flags/how-to-buy/eg.svg';
+import es from '../public/flags/how-to-buy/es.svg';
+import fr from '../public/flags/how-to-buy/fr.svg';
+import gh from '../public/flags/how-to-buy/gh.svg';
+import gr from '../public/flags/how-to-buy/gr.svg';
+import hk from '../public/flags/how-to-buy/hk.svg';
+import id from '../public/flags/how-to-buy/id.svg';
+import ind from '../public/flags/how-to-buy/in.svg';
+import it from '../public/flags/how-to-buy/it.svg';
+import ke from '../public/flags/how-to-buy/ke.svg';
+import lb from '../public/flags/how-to-buy/lb.svg';
+import lv from '../public/flags/how-to-buy/lv.svg';
+import mx from '../public/flags/how-to-buy/mx.svg';
+import my from '../public/flags/how-to-buy/my.svg';
+import ng from '../public/flags/how-to-buy/ng.svg';
+import pe from '../public/flags/how-to-buy/pe.svg';
+import pk from '../public/flags/how-to-buy/pk.svg';
+import ro from '../public/flags/how-to-buy/ro.svg';
+import th from '../public/flags/how-to-buy/th.svg';
+import tr from '../public/flags/how-to-buy/tr.svg';
+import uy from '../public/flags/how-to-buy/uy.svg';
+import ve from '../public/flags/how-to-buy/ve.svg';
+import za from '../public/flags/how-to-buy/za.svg';
+
 
 const CountrySelector = () => {
     return (
         <div>
-            <div className="w-full lg:w-4/5 mb-16 md:mb-32 m-auto border border-[#4A4C50] rounded-2xl transition-all duration-1000 hover:scale-105 backdrop-blur-lg">
-                <div className="w-full flex p-6 md:py-16 md:px-16 relative space-x-8 justify-center align-center">
-                    {/* <div className="flex-col space-y-4 w-full"></div>
-                    <div className="flex-col space-y-4 w-full"></div>
-                    <div className="flex-col space-y-4 w-full"></div> */}
 
+            <div className="w-full">
+                <p className="text-white text-center text-4xl mb-16 font-light">
+                    Choose your country below
+                </p></div>
+
+            <div className="w-full lg:w-4/5 mb-16 md:mb-32 m-auto border border-[#4A4C50] rounded-2xl lg:transition-all lg:duration-1000 lg:hover:scale-105 backdrop-blur-lg">
+
+                <div className="w-full flex flex-col p-6 space-y-8 justify-center items-center md:flex-row md:space-y-0 md:space-x-8 md:py-16 md:px-16 relative">
 
                     <div className="w-full flex flex-col space-y-8 items-center justify-center">
-
                         <div className="flex items-center">
                             <div className="">
                                 <Image
@@ -174,7 +210,6 @@ const CountrySelector = () => {
                     </div>
 
                     <div className="w-full flex flex-col space-y-8 items-center justify-center">
-
                         <div className="flex items-center">
                             <div className="">
                                 <Image
@@ -184,9 +219,11 @@ const CountrySelector = () => {
                                 />
                             </div>
                             <div>
-                                <p className="text-white text-3xl text-normal text-center mt-2 mr-2">
-                                    China
-                                </p>
+                                <Link href="https://app.openpeer.xyz" target="_blank">
+                                    <p className="text-white text-3xl text-normal text-center mt-2 mr-2">
+                                        China
+                                    </p>
+                                </Link>
                             </div>
                         </div>
                         <div className="flex items-center">
